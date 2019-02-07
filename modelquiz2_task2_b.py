e482e80666504f0f4c121517fd28a58dbf9098c9
Vijaisriraam <VijaiGopalS@dal.ca>
2019-02-06 20:58:13 -0400

    task2 2nd part

---
+++ modelquiz2_task2_b.py
@@ -0,0 +1,33 @@
+import csv
+def task2():
+    street = []
+    newstreet = []
+    reader1 = open('Street_Centrelines.csv', 'r')
+    reader2 = open('Bus_Stops.csv', 'r')
+    for r in reader1:
+        nread = r.split(",")
+        if nread[10] == "LOCAL STREET":
+            stree = nread[4].strip()
+            stree = stree.lower()
+            if stree not in street:
+                 street.append(stree)
+    for stre in street:
+         if stre not in newstreet:
+             newstreet.append(stre)    
+    metro = []
+    for st in newstreet:
+        for dir in reader2:
+            ndir = dir.split(",")
+            if ndir[7] == "Non-Standard":
+                stack = ndir[6].strip()
+                stack = stack.lower()
+                if stack.find(st) >= 0:
+                     metro.append(ndir[2])              
+    print("Stop Number    |    Location    |    FCODE")
+    for bus in metro:
+        for char in open('Bus_Stops.csv', 'r'):
+            newchar = char.split(",")
+            if newchar[2].find(bus) >= 0:
+                print(newchar[4],"",newchar[6],"",newchar[10])
+task2()
+
